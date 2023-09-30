ce588312be1b4a63fd0de0ca77ca534d0bd32f8e
itzp <prakharb2k6@gmail.com>
2023-09-30 20:55:13 +0530

    Checking out branch changes

--- test/main.py
+++ test/main.py
@@ -1,1 +1,23 @@
 # How to use git
+# 1. git init
+# 2. git add .
+# 3. git commit -m "message"
+# 4. git remote add origin
+# 5. git push -u origin master
+# 6. git pull origin master
+# 7. git clone
+# 8. git status
+# 9. git log
+# 10. git checkout
+# 11. git branch
+# 12. git merge <branch>
+# 13. git reset
+# 14. git revert
+# 15. git stash
+# 16. git pull --rebase origin master
+# 17. git switch <branch>
+# How to merge 2 branches
+# 1. git checkout master
+# 2. git merge <branch>
+# 3. git push origin master
+# How to revert a commit
